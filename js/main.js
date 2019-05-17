57776164285215d2031655d439cfa136e7118459
AlexeyStrekozov <44721000+AlexeyStrekozov@users.noreply.github.com>
2019-05-17 15:07:18 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,3 +10,78 @@ $(document).ready(function () {
     })
   })
 
+  $(document).ready(function(){
+    $('.js-ajax').on('submit', function(){
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: $("#form1").serialize(),
+            success: function(){
+                alert('Спсибо с вами свяжуться');
+            }
+            
+        });
+        $("#form1")[0].reset();
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.js-ajax2').on('submit', function(){
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: $("#form2").serialize(),
+            success: function(){
+                alert('Спсибо с вами свяжуться');
+            }
+        });
+        $("#form2")[0].reset();
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.js-ajax3').on('submit', function(){
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: $("#form3").serialize(),
+            success: function(){
+                alert('Спсибо с вами свяжуться');
+            }
+        });
+        $("#form3")[0].reset();
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.js-ajax4').on('submit', function(){
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: $("#form4").serialize(),
+            success: function(){
+                alert('Спсибо с вами свяжуться');
+            }
+        });
+        $("#form4")[0].reset();
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.js-ajaxModal').on('submit', function(){
+        $.ajax({
+            type: "POST",
+            url: "php/send.php",
+            data: $("#formModal").serialize(),
+            success: function(){
+                alert('Спсибо с вами свяжуться');
+            }
+        });
+        $("#formModal")[0].reset();
+        return false;
+    });
+});
